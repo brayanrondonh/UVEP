cd6dae1222d66ef824742807cfb2326d2a54545a
BrayanRondonH <brayanrondonh@gmail.com>
2018-09-14 20:57:47 -0500

    conexion a la base de datos lista

--- suscription.php
+++ suscription.php
@@ -1,5 +1,6 @@
 <?php 
 	$conexion = mysqli_connect("localhost","root","","unvenezolanoenperu") or die("Error en la conexion");
 
-	mysqli_query($conecion,"insert into suscription(email) values ('$_POST[email]')") or die("No se pudo registrar en la base de datos".mysqli_error($conexion));
+	mysqli_query($conexion,"insert into suscription(email) values ('$_POST[email]')") or die("No se pudo registrar en la base de datos".mysqli_error($conexion));
+	echo "Suscripcion realizada con exito";
 ?>
